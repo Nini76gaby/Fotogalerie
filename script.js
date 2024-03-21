4360e50708985af8e4769c5ac32dc050e1e6b1d1
Nini76gaby <gabydiana76@yahoo.de>
2024-03-21 17:52:14 +0100

    fullscreen section

---
+++ script.js
@@ -0,0 +1,31 @@
+let images=[
+    "img/bild1.jpg",
+    "img/bild4.jpg",
+    "img/bild8.jpg",
+    "img/bild13.jpg",
+    "img/bild17.jpg",
+    "img/bild5.jpg",
+    "img/bild7.jpg",
+    "img/bild9.jpg",
+    "img/bild10.jpg",
+    "img/bild14.jpg",
+    "img/bild15.jpg",
+    "img/bild18.jpg",
+    "img/bild19.jpg",
+    "img/bild23.jpg",
+    "img/bild27.jpg",
+    "img/bild3.jpg",
+    "img/bild28.jpg",
+    "img/bild5.jpg",
+    "img/bild16.jpg",
+    "img/bild20.jpg",
+    "img/bild25.jpg"
+];
+
+
+
+
+function render(){
+ let content= document.getElementById('content');
+ content.innerHTML='';
+}
